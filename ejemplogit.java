2b3fa17a717cbac403ffd0cf43feb0f76b5412f6
Natalia Mártir Moreno <nataliamartir@correo.ugr.es>
2025-01-14 12:34:18 +0100

    Archivo de java
    Archivo de ejemplo de java para el tema 2

---
+++ ejemplogit.java
@@ -0,0 +1,35 @@
+    rgaesgtbh
+    g
+    g
+    aqer
+    gae
+    g
+    g
+    g
+    aer
+    try (try (try (try (try (g
+    b
+    b
+    g
+    g
+    ) {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }) {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }) {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }) {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }) {
+        
+    } catch (Exception e) {
+        // TODO: handle exception
+    }
